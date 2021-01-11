431019e22606ac41868000d5001d6aec4bcb0cb1
TGH <54406116+MegaCrafter863@users.noreply.github.com>
2021-01-11 03:14:25 +0100

    Update gomuks.sh

--- installer/deb/gomuks.sh
+++ installer/deb/gomuks.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 sudo apt update -y
+echo "
+"
 sudo apt install git -y
+echo "
+"
 sudo apt install golang-go -y
+echo "
+"
 sudo apt install libolm-dev -y
+echo "
+"
 git clone https://github.com/tulir/gomuks.git
+echo "
+"
 cd gomuks
+echo "
+"
 go build
+echo "
+"
 sudo cp gomuks /bin
+echo "
+"
 cd ..
 gomuks
